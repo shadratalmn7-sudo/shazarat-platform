89c68dfc40cd63ca40246c7a68d16f1c1df641f2
shadratalmn7-sudo <shadrat.almn7@gmail.com>
2026-08-15 21:48:50 +0300

    Update nav-universal.js

--- nav-universal.js
+++ nav-universal.js
@@ -69,4 +69,5 @@
   menu.querySelectorAll('a').forEach((link) => link.addEventListener('click', () => setOpen(false)));
   document.addEventListener('keydown', (event) => { if (event.key === 'Escape') setOpen(false); });
   import('./nav-auth.js?v=2').catch(() => {});
+  import('./scholarship-extra.js?v=1').catch(() => {});
 })();
