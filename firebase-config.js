69cd0589d62131934ad9ebda2ebe8405dcc9c213
shadratalmn7-sudo <shadrat.almn7@gmail.com>
2026-08-15 18:17:22 +0300

    feat: add firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = Object.freeze({
+  apiKey: "AIzaSyA1EWlVkTThSIVwV7iOPIUwnWvB2tIg-yQ",
+  authDomain: "shazarat-platform.firebaseapp.com",
+  projectId: "shazarat-platform",
+  storageBucket: "shazarat-platform.firebasestorage.app",
+  messagingSenderId: "163989935844",
+  appId: "1:163989935844:web:c7b1a185da53076e6dbef1"
+});
